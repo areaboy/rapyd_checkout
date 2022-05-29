eb0dcb3af162e7333e15758de795b5c2613d8c3e
Esedo Fredrick <50068759+areaboy@users.noreply.github.com>
2022-05-29 17:23:57 +0100

    Update rapdy_access_keys.php

--- rapyd_checkout/rapdy_access_keys.php
+++ rapyd_checkout/rapdy_access_keys.php
@@ -2,8 +2,8 @@
 
                    // Sandbox Access Credentials
  $rapyd_base_url  = 'https://sandboxapi.rapyd.net';    //rapyd sandbox url.
- $access_key_call = '3E9F28224F4924A895B4';     // The access key received from Rapyd.
- $secret_key_call = '76b2336a4c884da467965c83e4a9268bb6c9c0f746f7bcea75f2d855650a6224ec8ef6c51c8e51c7';     // Never transmit the secret key by itself.
+ $access_key_call = 'your rapyd sandbox access key';     // The access key received from Rapyd.
+ $secret_key_call = 'rapyd secret key';     // Never transmit the secret key by itself.
 
 ?>
 
